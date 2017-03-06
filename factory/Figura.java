975426aba11bfdc9e74d710d27ac182e7812d0e3
maldad <canond_7@hotmail.com>
2017-03-06 14:58:19 -0600

    iniciando patrón factory

---
+++ factory/Figura.java
@@ -0,0 +1,20 @@
+public abstract class Figura {
+  protected String nombre;
+  protected Object valor;
+  protected int tamaño;
+  protected int posicionx;
+  protected int posiciony;
+
+  public Figura(){ 
+  
+  }
+  public void dibujar(){ //mostrar el elemento
+
+  }
+  public void trasladar(int x, int y){ 
+
+  }
+  public void escalar(int r){ 
+  
+  }
+}
---
+++ factory/Manipulador.java
@@ -0,0 +1,10 @@
+public abstract class Manipulador {
+  protected int largo;
+  protected int ancho;
+
+  public abstract Figura crearFigura(); //abstract methods cannot have a body
+
+  public void generaFigura(/*ubicacion, tipo*/){
+
+  }
+}
