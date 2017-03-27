192245c53f05e3b29325479f3bd7c99fa44e5c80
maldad <canond_7@hotmail.com>
2017-03-26 19:17:36 -0600

    ejemplo con interfaz y clase Observadora ya incluida en java

--- observer/datos_java_built_in/Main.java
+++ observer/datos_java_built_in/Main.java
@@ -5,7 +5,17 @@ public class Main {
     ConjuntoDatos s1 = new ConjuntoDatos();
     //crear observador
     Observer o1 = new Imprimir(s1);
+    Observer o2 = new Promediar(s1);
+    Observer o3 = new Graficar(s1);
+    //es como si los fuera apilando, hm, interesante...
+    s1.addObserver(o3);
+    s1.addObserver(o2);
     s1.addObserver(o1);
+
+
     s1.agregar(1);
+    s1.agregar(2);
+    s1.agregar(5);
+    s1.agregar(10);
   }
 }
