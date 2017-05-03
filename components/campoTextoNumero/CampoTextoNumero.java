227f9993b8aca7bbcf94adac7a088af404f53b51
maldad <canond_7@hotmail.com>
2017-05-03 14:59:31 -0500

    agregando una regex que valida cuando un número es decimal, falta setear la cantidad de decimales que se quiera

--- components/campoTextoNumero/CampoTextoNumero.java
+++ components/campoTextoNumero/CampoTextoNumero.java
@@ -1,9 +1,11 @@
 import javax.swing.*;
 import java.awt.event.*;
+import java.util.regex.Pattern;
 
 public class CampoTextoNumero extends JTextField implements KeyListener {
   private int limMax;
   private int limMin;
+  private int decimales;
 
   public CampoTextoNumero(){
     super();
@@ -17,18 +19,32 @@ public class CampoTextoNumero extends JTextField implements KeyListener {
       public boolean verify(JComponent input) {
         JTextField tf = (JTextField) input;
         String cadena = tf.getText();
+        if(!Pattern.matches("\\d*\\.?\\d+", cadena)){
+          System.out.println("ingrese un solo punto decimal");
+          return false;
+        }
         if(cadena.length() == 0){
           System.out.println("ingrese un número entre: " + getLimMin() + " y " + getLimMax());
           return false;
         }
-        if(Integer.parseInt(cadena) < getLimMin()){
-          System.out.println("debe ingresar un número no menor a: " + getLimMin());
-          return false;
-        }
-        if(Integer.parseInt(cadena) > getLimMax()){
-          System.out.println("debe ingresar un número no mayor a: " + getLimMax());
-          return false;
-        }
+        try{
+          if(Double.parseDouble(cadena) < (double)getLimMin()){
+            System.out.println("debe ingresar un número mayor a: " + getLimMin());
+            return false;
+          }
+          if(Double.parseDouble(cadena) > (double)getLimMax()){
+            System.out.println("debe ingresar un número menor a: " + getLimMax());
+            return false;
+          }
+          if(Integer.parseInt(cadena) < getLimMin()){
+            System.out.println("debe ingresar un número mayor a: " + getLimMin());
+            return false;
+          }
+          if(Integer.parseInt(cadena) > getLimMax()){
+            System.out.println("debe ingresar un número menor a: " + getLimMax());
+            return false;
+          }
+        } catch(Exception e){}
         return true;
       }
     });
@@ -46,7 +62,7 @@ public class CampoTextoNumero extends JTextField implements KeyListener {
   public int getLimMax(){
     return limMax;
   }
- 
+
   public int getLimMin(){
     return limMin;
   }
@@ -64,13 +80,12 @@ public class CampoTextoNumero extends JTextField implements KeyListener {
 
   @Override
   public void keyPressed(KeyEvent e){
-
   }
 
   @Override
   public void keyTyped(KeyEvent e) {
     char caracter = e.getKeyChar();
-    if(caracter < '0' || caracter > '9'){
+    if((caracter < '0' || caracter > '9') && caracter != '.'){
       e.consume();
     }
   }
